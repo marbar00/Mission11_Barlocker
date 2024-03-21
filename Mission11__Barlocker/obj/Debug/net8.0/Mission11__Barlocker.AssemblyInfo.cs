34b4edbca35c21c178a3cca36744d32d1f1ff297
marbar00 <barlockerm@gmail.com>
2024-03-21 16:20:22 -0600

    Added database

--- Mission11__Barlocker/obj/Debug/net8.0/Mission11__Barlocker.AssemblyInfo.cs
+++ Mission11__Barlocker/obj/Debug/net8.0/Mission11__Barlocker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11__Barlocker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0140a76356a905db5e86917f26813d615b99a6cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11__Barlocker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11__Barlocker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
