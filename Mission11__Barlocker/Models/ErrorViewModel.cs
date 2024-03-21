0140a76356a905db5e86917f26813d615b99a6cf
marbar00 <barlockerm@gmail.com>
2024-03-21 15:00:21 -0600

    Created project

---
+++ Mission11__Barlocker/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Mission11__Barlocker.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
